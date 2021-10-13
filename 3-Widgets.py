e567bbd6dd680a1dc811f0804e59f11c15306753
Versun <versun@versun.org>
2021-10-13 09:02:47 +0800

    done 3

--- 3-Widgets.py
+++ 3-Widgets.py
@@ -219,9 +219,61 @@ class MainWindow_6(QMainWindow): # QSpinBox and QDoubleSpinBox
     def value_changed_str(self,s):
         print(s)
 
-
+class MainWindow_7(QMainWindow): # QSlider
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("My App")
+        
+        widget = QSlider()
+        # or: widget = QSlider(Qt.Vertical)
+        # or: widget = QSlider(Qt.Horizontal)
+        widget.setMinimum(-10)
+        widget.setMaximum(3)
+        # Or: widget.setRange(-10,3)
+        
+        widget.setSingleStep(3)
+        
+        widget.valueChanged.connect(self.value_changed)
+        widget.sliderMoved.connect(self.slider_position)
+        widget.sliderPressed.connect(self.slider_pressed)
+        widget.sliderReleased.connect(self.slider_released)
+        
+        self.setCentralWidget(widget)
+    def value_changed(self,i):
+        print(i)
+    def slider_position(self,p):
+        print("position", p)
+    def slider_pressed(self):
+        print("Pressed")
+    def slider_released(self):
+        print("Released")
+        
+class MainWindow_8(QMainWindow): # QDial
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("My APP")
+        
+        widget = QDial()
+        widget.setRange(-10, 100)
+        widget.setSingleStep(0.5)
+        
+        widget.valueChanged.connect(self.value_changed)
+        widget.sliderMoved.connect(self.slider_position)
+        widget.sliderPressed.connect(self.slider_pressed)
+        widget.sliderReleased.connect(self.slider_released)
+        
+        self.setCentralWidget(widget)
+    def value_changed(self,i):
+        print(i)
+    def slider_position(self,p):
+        print("position", p)
+    def slider_pressed(self):
+        print("Pressed!")
+    def slider_released(self):
+        print("Released")
+        
 app = QApplication(sys.argv)
-window = MainWindow_6()
+window = MainWindow_8()
 window.show()
 
 app.exec()
